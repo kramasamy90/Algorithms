22e98e0566f8d59f0d4657feafb320c85d749634
Ramasamy Kandasamy <kramasamy90@gmail.com>
2022-07-09 19:24:51 +0530

    Initial commit for array_rotation.cpp

---
+++ uncat/array_rotation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+Given an array of integers a[] rotate k times.
+Eg: a = {1, 2, 3, 4, 5} rotated 2 times gives {4, 5, 1, 2, 3}.
+**/
+
+/**
+ * Algorithm-1: Time complexity = O(n), space complexity = O(n).
+ * 
+ */
+
+void rotate_1(vector<int>& a, int k){
+	
+}
